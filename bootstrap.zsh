5c9965fdaf8b8945883440e9577233ea1419b6bb
juan gabriel ramirez <tuxgabo@gmail.com>
2020-12-12 11:08:13 -0500

    prevent world expansion in zsh

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -15,7 +15,7 @@ function doIt() {
 	source ~/.zshrc;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" "==" "--force" -o "$1" "==" "-f" ]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
