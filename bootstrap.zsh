bd0f6a56092834117399fa6d7819d8c918cdd1a8
juan gabriel ramirez <tuxgabo@gmail.com>
2020-12-13 14:09:33 -0500

    bootstrap script fixed

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -18,7 +18,7 @@ function doIt() {
   if [ "$1" "==" "--force" -o "$1" "==" "-f" ]; then
     doIt;
   else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    read "REPLY?This may overwrite existing files in your home directory. Are you sure? (y/n) ?"
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
       doIt;
