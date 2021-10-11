4ffac838c8f3e299dc9b328a89b995ca65380a5c
Juan Ramirez <tuxgabo@gmail.com>
2021-10-11 10:29:25 -0500

    perf(dotfiles): setup with ()

--- neovim/.config/nvim/lua/lsp_config/init.lua
+++ neovim/.config/nvim/lua/lsp_config/init.lua
@@ -1,4 +1,4 @@
--- General 
+-- General
 --Decrease update time
 vim.o.foldlevelstart = 4
 vim.o.updatetime = 250
@@ -88,13 +88,15 @@ end
 -- map buffer local keybindings when the language server attaches
 local servers = { 'tsserver', 'tailwindcss' }
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    on_attach = on_attach,
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-    flags = {
-      debounce_text_changes = 150,
+  nvim_lsp[lsp].setup(
+    {
+      on_attach = on_attach,
+      capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+      flags = {
+        debounce_text_changes = 150,
+      }
     }
-  }
+  )
 end
 ----------------- LUA LSP Server --------------------
 local runtime_path = vim.split(package.path, ';')
@@ -103,212 +105,226 @@ table.insert(runtime_path, "lua/?/init.lua")
 local sumneko_root_path = vim.fn.getenv 'HOME' .. '/.code/lua-language-server' -- custom folder ~/.code were lsp client was cloned and built
 local sumneko_binary = sumneko_root_path .. '/bin/macOS/lua-language-server'
 
-nvim_lsp.sumneko_lua.setup {
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
+nvim_lsp.sumneko_lua.setup(
+  {
+    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Setup your lua path
+          path = runtime_path,
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
       },
     },
-  },
-  on_attach = on_attach
-}
+    on_attach = on_attach
+  }
+)
 ----------------------- nvim treesitter ---------------------
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
+require'nvim-treesitter.configs'.setup(
+  {
+    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    highlight = {
+      enable = true,              -- false will disable the whole extension
     },
-  },
-  indent = {
-    enable = true
-  },
-  textobjects = {
-    select = {
+    incremental_selection = {
       enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
       keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
       },
     },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
+    indent = {
+      enable = true
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        },
       },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']]'] = '@function.outer',
+          [']m'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[['] = '@function.outer',
+          ['[m'] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
       },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
+      swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
       },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
+      lsp_interop = {
+        enable = true,
+        border = 'none',
+        peek_definition_code = {
+          ["<leader>df"] = "@function.outer",
+          ["<leader>dF"] = "@class.outer",
+        },
       },
     },
-    swap = {
-      enable = true,
-      swap_next = {
-        ["<leader>a"] = "@parameter.inner",
-      },
-      swap_previous = {
-        ["<leader>A"] = "@parameter.inner",
-      },
-    },
-    lsp_interop = {
-      enable = true,
-      border = 'none',
-      peek_definition_code = {
-        ["<leader>df"] = "@function.outer",
-        ["<leader>dF"] = "@class.outer",
-      },
-    },
-  },
-}
+  }
+)
 vim.api.nvim_exec([[
 set foldmethod=expr
 set foldexpr=nvim_treesitter#foldexpr()
 ]], true)
 ------------------------ telescope nvim ------------------------------
-require('telescope').setup{
-  defaults = {
-    layout_strategy = 'vertical'
-  },
-  pickers = {
-  },
-  extensions = {
-  }
-}
--------------------------nvim-web-devicons-------------------------------
-require'nvim-web-devicons'.setup {
-  -- your personnal icons can go here (to override)
-  -- DevIcon will be appended to `name`
-  override = {
-    zsh = {
-      icon = "",
-      color = "#428850",
-      name = "Zsh"
+require('telescope').setup(
+  {
+    defaults = {
+      layout_strategy = 'vertical'
+    },
+    pickers = {
+    },
+    extensions = {
     }
-  };
-  -- globally enable default icons (default to false)
-  -- will get overriden by `get_icons` option
-  default = true;
-}
+  }
+)
+-------------------------nvim-web-devicons-------------------------------
+require'nvim-web-devicons'.setup(
+  {
+    -- your personnal icons can go here (to override)
+    -- DevIcon will be appended to `name`
+    override = {
+      zsh = {
+        icon = "",
+        color = "#428850",
+        name = "Zsh"
+      }
+    };
+    -- globally enable default icons (default to false)
+    -- will get overriden by `get_icons` option
+    default = true;
+  }
+)
 ------------------------------nvim-tree.lua ----------------------------
 -- following options are the default
-require'nvim-tree'.setup {
-  -- disables netrw completely
-  disable_netrw       = true,
-  -- hijack netrw window on startup
-  hijack_netrw        = true,
-  -- open the tree when running this setup function
-  open_on_setup       = false,
-  -- will not open on setup if the filetype is in this list
-  ignore_ft_on_setup  = {},
-  -- closes neovim automatically when the tree is the last **WINDOW** in the view
-  auto_close          = false,
-  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab         = false,
-  -- hijacks new directory buffers when they are opened.
-  update_to_buf_dir   = {
-    -- enable the feature
-    enable = true,
-    -- allow to open the tree if it was previously closed
-    auto_open = true,
-  },
-  -- hijack the cursor in the tree to put it at the start of the filename
-  hijack_cursor       = false,
-  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-  update_cwd          = false,
-  -- show lsp diagnostics in the signcolumn
-  lsp_diagnostics     = true,
-  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-  update_focused_file = {
-    -- enables the feature
-    enable      = false,
-    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-    -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = false,
-    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-    ignore_list = {}
-  },
-  view = {
-    -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
-    width = 30,
-    -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
-    height = 30,
-    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-    side = 'left',
-    -- if true the tree will resize itself after opening a file
-    auto_resize = true,
-    mappings = {
-      -- custom only false will merge the list with the default mappings
-      -- if true, it will only use your list to set the mappings
-      custom_only = false,
-      -- list of mappings to set on the tree manually
-      list = {}
+require'nvim-tree'.setup(
+  {
+    -- disables netrw completely
+    disable_netrw       = true,
+    -- hijack netrw window on startup
+    hijack_netrw        = true,
+    -- open the tree when running this setup function
+    open_on_setup       = false,
+    -- will not open on setup if the filetype is in this list
+    ignore_ft_on_setup  = {},
+    -- closes neovim automatically when the tree is the last **WINDOW** in the view
+    auto_close          = false,
+    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+    open_on_tab         = false,
+    -- hijacks new directory buffers when they are opened.
+    update_to_buf_dir   = {
+      -- enable the feature
+      enable = true,
+      -- allow to open the tree if it was previously closed
+      auto_open = true,
+    },
+    -- hijack the cursor in the tree to put it at the start of the filename
+    hijack_cursor       = false,
+    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+    update_cwd          = false,
+    -- show lsp diagnostics in the signcolumn
+    diagnostics     ={
+      enable = true
+    },
+    -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+    update_focused_file = {
+      -- enables the feature
+      enable      = false,
+      -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+      -- only relevant when `update_focused_file.enable` is true
+      update_cwd  = false,
+      -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+      -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+      ignore_list = {}
+    },
+    view = {
+      -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
+      width = 30,
+      -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
+      height = 30,
+      -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+      side = 'left',
+      -- if true the tree will resize itself after opening a file
+      auto_resize = true,
+      mappings = {
+        -- custom only false will merge the list with the default mappings
+        -- if true, it will only use your list to set the mappings
+        custom_only = false,
+        -- list of mappings to set on the tree manually
+        list = {}
+      }
     }
   }
-}
+)
 ---------------------- lualine.nvim -------------------------------------
-require'lualine'.setup {
-  options = {
-    icons_enabled = true,
-    theme = 'gruvbox_light',
-    component_separators = {'', ''},
-    section_separators = {'', ''},
-    disabled_filetypes = {}
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {}
-}
+require'lualine'.setup(
+  {
+    options = {
+      icons_enabled = true,
+      theme = 'gruvbox_light',
+      component_separators = {'', ''},
+      section_separators = {'', ''},
+      disabled_filetypes = {}
+    },
+    sections = {
+      lualine_a = {'mode'},
+      lualine_b = {'branch'},
+      lualine_c = {'filename'},
+      lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_y = {'progress'},
+      lualine_z = {'location'}
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {'filename'},
+      lualine_x = {'location'},
+      lualine_y = {},
+      lualine_z = {}
+    },
+    tabline = {},
+    extensions = {}
+  }
+)
