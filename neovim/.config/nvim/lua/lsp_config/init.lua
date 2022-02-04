65b55b127013d441b32bdf7cd4a38790c389e786
Juan Ramirez <tuxgabo@gmail.com>
2022-02-04 10:16:57 -0500

    fixed autocomplete shortcut

--- neovim/.config/nvim/lua/lsp_config/init.lua
+++ neovim/.config/nvim/lua/lsp_config/init.lua
@@ -593,7 +593,7 @@ cmp.setup({
   mapping = {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-x>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
