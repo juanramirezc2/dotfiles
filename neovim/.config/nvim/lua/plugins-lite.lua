ee40669a89224a2ff6d3df944e13ffe4686fbee2
Juan Ramirez <tuxgabo@gmail.com>
2022-07-09 10:47:39 -0500

    better tree sitter mappins

--- neovim/.config/nvim/lua/plugins-lite.lua
+++ neovim/.config/nvim/lua/plugins-lite.lua
@@ -166,10 +166,11 @@ require('nvim-treesitter.configs').setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm',
+      init_selection = '<c-space>',
+      node_incremental = '<c-space>',
+      -- TODO: I'm not sure for this one.
+      scope_incremental = '<c-s>',
+      node_decremental = '<c-backspace>',
     },
   },
   indent = {
