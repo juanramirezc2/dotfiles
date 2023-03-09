f5c53ff627774bff68ec47338f6f1b3ca546b67b
Juan Ramirez <jgramirezc2@gmail.com>
2023-03-09 12:20:55 -0500

    formatting sync from null ls wiki

--- neovim/.config/nvim/lua/plugins-lite.lua
+++ neovim/.config/nvim/lua/plugins-lite.lua
@@ -696,10 +696,7 @@ require('indent_blankline').setup {
 local null_ls = require("null-ls")
 vim.cmd('map <Leader>ln :NullLsInfo<CR>')
 -- format on save using lsp format
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-local event = "BufWritePre" -- or "BufWritePost"
-local async = event == "BufWritePost"
-
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 require("null-ls").setup({
   sources = {
     null_ls.builtins.code_actions.eslint_d,
@@ -710,26 +707,14 @@ require("null-ls").setup({
   -- you can reuse a shared lspconfig on_attach callback here
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
-      vim.keymap.set("n", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
-
-      -- format on save
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-      vim.api.nvim_create_autocmd(event, {
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
         buffer = bufnr,
-        group = group,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+          vim.lsp.buf.format({ bufnr = bufnr })
         end,
-        desc = "[lsp] format on save",
       })
-    end
-
-    if client.supports_method("textDocument/rangeFormatting") then
-      vim.keymap.set("x", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
     end
   end,
 })
