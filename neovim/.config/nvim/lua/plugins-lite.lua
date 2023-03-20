3285d7c7ad9776c3cb4592bc05df66ab9b6d21a6
Juan Ramirez <jgramirezc2@gmail.com>
2023-03-20 09:55:37 -0500

    noice more notizable

--- neovim/.config/nvim/lua/plugins-lite.lua
+++ neovim/.config/nvim/lua/plugins-lite.lua
@@ -990,12 +990,44 @@ require("noice").setup({
   },
   -- you can enable a preset for easier configuration
   presets = {
-    bottom_search = true, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
+  views = {
+      cmdline_popup = {
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+        border = {
+          style = "double",
+          padding = { 1, 2 },
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+        border = {
+          style = "double",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
+    },
 })
 
 -- bufdelete.nvim mappings
