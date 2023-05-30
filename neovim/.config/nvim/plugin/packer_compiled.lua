1da7d91ae4b04f2ebff77b6cdc3d25543912c030
Juan Ramirez <jugaramirez@utp.edu.co>
2023-05-30 11:57:34 -0500

    lsp saga for better lsp dialogs

--- neovim/.config/nvim/plugin/packer_compiled.lua
+++ neovim/.config/nvim/plugin/packer_compiled.lua
@@ -174,6 +174,15 @@ _G.packer_plugins = {
     path = "/Users/jgramirez/.local/share/nvim/site/pack/packer/start/lspkind.nvim",
     url = "https://github.com/onsails/lspkind.nvim"
   },
+  ["lspsaga.nvim"] = {
+    after = { "nvim-web-devicons" },
+    config = { "\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\flspsaga\frequire\0" },
+    loaded = false,
+    needs_bufread = false,
+    only_cond = false,
+    path = "/Users/jgramirez/.local/share/nvim/site/pack/packer/opt/lspsaga.nvim",
+    url = "https://github.com/glepnir/lspsaga.nvim"
+  },
   ["lualine.nvim"] = {
     loaded = true,
     path = "/Users/jgramirez/.local/share/nvim/site/pack/packer/start/lualine.nvim",
@@ -283,8 +292,12 @@ _G.packer_plugins = {
     url = "https://github.com/windwp/nvim-ts-autotag"
   },
   ["nvim-web-devicons"] = {
-    loaded = true,
-    path = "/Users/jgramirez/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    load_after = {
+      ["lspsaga.nvim"] = true
+    },
+    loaded = false,
+    needs_bufread = false,
+    path = "/Users/jgramirez/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["octo.nvim"] = {
@@ -422,10 +435,6 @@ time([[Setup for vim-matchup]], false)
 time([[packadd for vim-matchup]], true)
 vim.cmd [[packadd vim-matchup]]
 time([[packadd for vim-matchup]], false)
--- Config for: nvim-cmp
-time([[Config for nvim-cmp]], true)
-try_loadstring("\27LJ\2\n\2\0\0\n\0\n\2(6\0\0\0009\0\1\0009\0\2\0)\2\0\0'\3\3\0B\0\3\2\a\0\4\0X\0\2€+\0\1\0L\0\2\0006\0\5\0006\2\0\0009\2\1\0029\2\6\2)\4\0\0B\2\2\0A\0\0\3\b\1\0\0X\2\17€6\2\0\0009\2\1\0029\2\a\2)\4\0\0\23\5\1\0)\6\0\0\23\a\1\0\18\b\1\0004\t\0\0B\2\a\2:\2\1\2\18\4\2\0009\2\b\2'\5\t\0B\2\3\2\n\2\0\0X\2\2€+\2\1\0X\3\1€+\2\2\0L\2\2\0\n^%s*$\nmatch\22nvim_buf_get_text\24nvim_win_get_cursor\vunpack\vprompt\fbuftype\24nvim_buf_get_option\bapi\bvim\0\2p\2\0\4\3\4\0\15-\0\0\0009\0\0\0B\0\1\2\15\0\0\0X\1\a€9\1\1\0009\1\2\1\a\1\3\0X\1\3€-\1\1\0G\3\0\0C\1\0\0-\1\2\0G\3\0\0C\1\0\0\0À\5À\4À\fcopilot\tname\vsource\23get_selected_entryš\1\0\1\5\2\6\0\21-\1\0\0009\1\0\1B\1\1\2\15\0\1\0X\2\r€-\1\1\0B\1\1\2\15\0\1\0X\2\t€-\1\0\0009\1\1\0015\3\4\0-\4\0\0009\4\2\0049\4\3\4=\4\5\3B\1\2\1X\1\2€\18\1\0\0B\1\1\1K\0\1\0\0À\3À\rbehavior\1\0\0\vSelect\19SelectBehavior\21select_next_item\fvisibleR\0\1\3\1\2\0\f-\1\0\0009\1\0\1B\1\1\2\15\0\1\0X\2\4€-\1\0\0009\1\1\1B\1\1\1X\1\2€\18\1\0\0B\1\1\1K\0\1\0\0À\21select_prev_item\fvisibleë\n\1\0\16\0L\0œ\0016\0\0\0'\2\1\0B\0\2\0026\1\0\0'\3\2\0B\1\2\0029\2\3\0\18\4\2\0009\2\4\2'\5\5\0009\6\6\1B\6\1\0A\2\2\0016\2\0\0'\4\a\0B\2\2\0023\3\b\0009\4\t\0009\4\n\0045\6\v\0B\4\2\0029\5\t\0009\5\n\0055\a\f\0009\b\r\0009\b\14\b=\b\15\aB\5\2\0029\6\16\0005\b-\0009\t\t\0009\t\17\t9\t\18\t5\v\23\0009\f\t\0009\f\19\f5\14\22\0009\15\20\0009\15\21\15=\15\15\14B\f\2\2=\f\24\v9\f\t\0009\f\25\f5\14\26\0009\15\20\0009\15\21\15=\15\15\14B\f\2\2=\f\27\v9\f\t\0009\f\28\f)\14üÿB\f\2\2=\f\29\v9\f\t\0009\f\28\f)\14\4\0B\f\2\2=\f\30\v9\f\t\0009\f\31\fB\f\1\2=\f \v9\f\t\0009\f!\fB\f\1\2=\f\"\v3\f#\0=\f$\v9\f\t\0009\f\n\f5\14%\0009\15\r\0009\15\14\15=\15\15\14B\f\2\2=\f&\v9\f\t\0003\14'\0005\15(\0B\f\3\2=\f)\v9\f\t\0003\14*\0005\15+\0B\f\3\2=\f,\vB\t\2\2=\t\t\b4\t\3\0005\n.\0>\n\1\t5\n/\0>\n\2\t=\t0\b5\t5\0009\n1\0025\f2\0005\r3\0=\r4\fB\n\2\2=\n6\t=\t7\b5\t9\0005\n8\0=\n:\t5\n;\0004\v\v\0006\f\0\0'\14<\0B\f\2\0029\f=\f>\f\1\v9\f>\0009\f?\f9\f@\f>\f\2\v9\f>\0009\f?\f9\fA\f>\f\3\v9\f>\0009\f?\f9\fB\f>\f\4\v9\f>\0009\f?\f9\fC\f>\f\5\v9\f>\0009\f?\f9\fD\f>\f\6\v9\f>\0009\f?\f9\fE\f>\f\a\v9\f>\0009\f?\f9\fF\f>\f\b\v9\f>\0009\f?\f9\fG\f>\f\t\v9\f>\0009\f?\f9\fH\f>\f\n\v=\vI\n=\nJ\t=\tK\bB\6\2\0012\0\0€K\0\1\0\17experimental\fsorting\16comparators\norder\vlength\14sort_text\tkind\rlocality\18recently_used\nscore\nexact\voffset\fcompare\vconfig\15prioritize\28copilot_cmp.comparators\1\0\1\20priority_weight\3\2\15ghost_text\1\0\0\1\0\1\rhl_group\16LspCodeLens\15formatting\vformat\1\0\0\15symbol_map\1\0\1\fCopilot\bï„“\1\0\3\18ellipsis_char\b...\rmaxwidth\0032\tmode\vsymbol\15cmp_format\fsources\1\0\2\tname\rnvim_lsp\16group_index\3\2\1\0\2\tname\fcopilot\16group_index\3\2\1\0\0\f<S-Tab>\1\3\0\0\6i\6s\0\n<Tab>\1\3\0\0\6i\6s\0\v<S-CR>\1\0\1\vselect\2\n<C-y>\0\n<C-e>\nabort\14<C-Space>\rcomplete\n<C-f>\n<C-b>\16scroll_docs\n<C-p>\1\0\0\21select_prev_item\n<C-n>\1\0\0\1\0\0\vInsert\19SelectBehavior\21select_next_item\vinsert\vpreset\nsetup\rbehavior\fReplace\20ConfirmBehavior\1\0\1\vselect\2\1\0\1\vselect\2\fconfirm\fmapping\0\flspkind\20on_confirm_done\17confirm_done\aon\nevent\"nvim-autopairs.completion.cmp\bcmp\frequire\0", "config", "nvim-cmp")
-time([[Config for nvim-cmp]], false)
 -- Config for: nvim-notify
 time([[Config for nvim-notify]], true)
 try_loadstring("\27LJ\2\n@\0\0\3\0\5\1\a6\0\0\0009\0\1\0006\2\2\0009\2\3\0029\2\4\2\24\2\0\2D\0\2\0\nlines\6o\bvim\nfloor\tmath\1€€ ÿ\3B\0\0\3\0\5\1\a6\0\0\0009\0\1\0006\2\2\0009\2\3\0029\2\4\2\24\2\0\2D\0\2\0\fcolumns\6o\bvim\nfloor\tmath\1€€ ÿ\3w\1\0\5\0\b\0\v6\0\0\0'\2\1\0B\0\2\0029\1\2\0005\3\3\0003\4\4\0=\4\5\0033\4\6\0=\4\a\3B\1\2\1K\0\1\0\14max_width\0\15max_height\0\1\0\2\ftimeout\3¸\23\rtop_down\2\nsetup\vnotify\frequire\0", "config", "nvim-notify")
@@ -434,6 +443,10 @@ time([[Config for nvim-notify]], false)
 time([[Config for nvim-colorizer.lua]], true)
 try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14colorizer\frequire\0", "config", "nvim-colorizer.lua")
 time([[Config for nvim-colorizer.lua]], false)
+-- Config for: nvim-cmp
+time([[Config for nvim-cmp]], true)
+try_loadstring("\27LJ\2\n\2\0\0\n\0\n\2(6\0\0\0009\0\1\0009\0\2\0)\2\0\0'\3\3\0B\0\3\2\a\0\4\0X\0\2€+\0\1\0L\0\2\0006\0\5\0006\2\0\0009\2\1\0029\2\6\2)\4\0\0B\2\2\0A\0\0\3\b\1\0\0X\2\17€6\2\0\0009\2\1\0029\2\a\2)\4\0\0\23\5\1\0)\6\0\0\23\a\1\0\18\b\1\0004\t\0\0B\2\a\2:\2\1\2\18\4\2\0009\2\b\2'\5\t\0B\2\3\2\n\2\0\0X\2\2€+\2\1\0X\3\1€+\2\2\0L\2\2\0\n^%s*$\nmatch\22nvim_buf_get_text\24nvim_win_get_cursor\vunpack\vprompt\fbuftype\24nvim_buf_get_option\bapi\bvim\0\2p\2\0\4\3\4\0\15-\0\0\0009\0\0\0B\0\1\2\15\0\0\0X\1\a€9\1\1\0009\1\2\1\a\1\3\0X\1\3€-\1\1\0G\3\0\0C\1\0\0-\1\2\0G\3\0\0C\1\0\0\0À\5À\4À\fcopilot\tname\vsource\23get_selected_entryš\1\0\1\5\2\6\0\21-\1\0\0009\1\0\1B\1\1\2\15\0\1\0X\2\r€-\1\1\0B\1\1\2\15\0\1\0X\2\t€-\1\0\0009\1\1\0015\3\4\0-\4\0\0009\4\2\0049\4\3\4=\4\5\3B\1\2\1X\1\2€\18\1\0\0B\1\1\1K\0\1\0\0À\3À\rbehavior\1\0\0\vSelect\19SelectBehavior\21select_next_item\fvisibleR\0\1\3\1\2\0\f-\1\0\0009\1\0\1B\1\1\2\15\0\1\0X\2\4€-\1\0\0009\1\1\1B\1\1\1X\1\2€\18\1\0\0B\1\1\1K\0\1\0\0À\21select_prev_item\fvisibleë\n\1\0\16\0L\0œ\0016\0\0\0'\2\1\0B\0\2\0026\1\0\0'\3\2\0B\1\2\0029\2\3\0\18\4\2\0009\2\4\2'\5\5\0009\6\6\1B\6\1\0A\2\2\0016\2\0\0'\4\a\0B\2\2\0023\3\b\0009\4\t\0009\4\n\0045\6\v\0B\4\2\0029\5\t\0009\5\n\0055\a\f\0009\b\r\0009\b\14\b=\b\15\aB\5\2\0029\6\16\0005\b-\0009\t\t\0009\t\17\t9\t\18\t5\v\23\0009\f\t\0009\f\19\f5\14\22\0009\15\20\0009\15\21\15=\15\15\14B\f\2\2=\f\24\v9\f\t\0009\f\25\f5\14\26\0009\15\20\0009\15\21\15=\15\15\14B\f\2\2=\f\27\v9\f\t\0009\f\28\f)\14üÿB\f\2\2=\f\29\v9\f\t\0009\f\28\f)\14\4\0B\f\2\2=\f\30\v9\f\t\0009\f\31\fB\f\1\2=\f \v9\f\t\0009\f!\fB\f\1\2=\f\"\v3\f#\0=\f$\v9\f\t\0009\f\n\f5\14%\0009\15\r\0009\15\14\15=\15\15\14B\f\2\2=\f&\v9\f\t\0003\14'\0005\15(\0B\f\3\2=\f)\v9\f\t\0003\14*\0005\15+\0B\f\3\2=\f,\vB\t\2\2=\t\t\b4\t\3\0005\n.\0>\n\1\t5\n/\0>\n\2\t=\t0\b5\t5\0009\n1\0025\f2\0005\r3\0=\r4\fB\n\2\2=\n6\t=\t7\b5\t9\0005\n8\0=\n:\t5\n;\0004\v\v\0006\f\0\0'\14<\0B\f\2\0029\f=\f>\f\1\v9\f>\0009\f?\f9\f@\f>\f\2\v9\f>\0009\f?\f9\fA\f>\f\3\v9\f>\0009\f?\f9\fB\f>\f\4\v9\f>\0009\f?\f9\fC\f>\f\5\v9\f>\0009\f?\f9\fD\f>\f\6\v9\f>\0009\f?\f9\fE\f>\f\a\v9\f>\0009\f?\f9\fF\f>\f\b\v9\f>\0009\f?\f9\fG\f>\f\t\v9\f>\0009\f?\f9\fH\f>\f\n\v=\vI\n=\nJ\t=\tK\bB\6\2\0012\0\0€K\0\1\0\17experimental\fsorting\16comparators\norder\vlength\14sort_text\tkind\rlocality\18recently_used\nscore\nexact\voffset\fcompare\vconfig\15prioritize\28copilot_cmp.comparators\1\0\1\20priority_weight\3\2\15ghost_text\1\0\0\1\0\1\rhl_group\16LspCodeLens\15formatting\vformat\1\0\0\15symbol_map\1\0\1\fCopilot\bï„“\1\0\3\18ellipsis_char\b...\rmaxwidth\0032\tmode\vsymbol\15cmp_format\fsources\1\0\2\tname\rnvim_lsp\16group_index\3\2\1\0\2\tname\fcopilot\16group_index\3\2\1\0\0\f<S-Tab>\1\3\0\0\6i\6s\0\n<Tab>\1\3\0\0\6i\6s\0\v<S-CR>\1\0\1\vselect\2\n<C-y>\0\n<C-e>\nabort\14<C-Space>\rcomplete\n<C-f>\n<C-b>\16scroll_docs\n<C-p>\1\0\0\21select_prev_item\n<C-n>\1\0\0\1\0\0\vInsert\19SelectBehavior\21select_next_item\vinsert\vpreset\nsetup\rbehavior\fReplace\20ConfirmBehavior\1\0\1\vselect\2\1\0\1\vselect\2\fconfirm\fmapping\0\flspkind\20on_confirm_done\17confirm_done\aon\nevent\"nvim-autopairs.completion.cmp\bcmp\frequire\0", "config", "nvim-cmp")
+time([[Config for nvim-cmp]], false)
 
 -- Command lazy-loads
 time([[Defining lazy-load commands]], true)
@@ -450,6 +463,7 @@ vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Event lazy-loads
 time([[Defining lazy-load event autocommands]], true)
+vim.cmd [[au LspAttach * ++once lua require("packer.load")({'lspsaga.nvim'}, { event = "LspAttach *" }, _G.packer_plugins)]]
 vim.cmd [[au InsertEnter * ++once lua require("packer.load")({'copilot.lua'}, { event = "InsertEnter *" }, _G.packer_plugins)]]
 time([[Defining lazy-load event autocommands]], false)
 vim.cmd("augroup END")
