f28da80343dcfb07c9887615bfe1ba3bba8c14b2
Juan Gabriel Ramirez <juanramirez@Juans-MacBook-Pro-2.local>
2025-09-19 11:17:12 -0500

    seems like new lazy vim comes with a tree plugin

--- neovim/.config/nvim/lua/plugins/neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,29 +1,29 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    -- opts will be merged with the parent spec
-    opts = {
-      filesystem = {
-        bind_to_cwd = true, -- true creates a 2-way binding between vim's cwd and neo-tree's root
-        cwd_target = {
-          sidebar = "tab", -- sidebar is when position = left or right
-          current = "window", -- current is when position = current
-        },
-      },
-      window = {
-        width = 50,
-        mappings = {
-          ["<space>"] = false, -- disable space until we figure out which-key disabling
-          ["l"] = "open",
-          [";"] = "toggle_preview",
-          ["o"] = "open",
-          ["h"] = "close_node",
-        },
-        fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
-          ["<C-j>"] = "move_cursor_down",
-          ["<C-k>"] = "move_cursor_up",
-        },
-      },
-    },
-  },
+  -- {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   -- opts will be merged with the parent spec
+  --   opts = {
+  --     filesystem = {
+  --       bind_to_cwd = true, -- true creates a 2-way binding between vim's cwd and neo-tree's root
+  --       cwd_target = {
+  --         sidebar = "tab", -- sidebar is when position = left or right
+  --         current = "window", -- current is when position = current
+  --       },
+  --     },
+  --     window = {
+  --       width = 50,
+  --       mappings = {
+  --         ["<space>"] = false, -- disable space until we figure out which-key disabling
+  --         ["l"] = "open",
+  --         [";"] = "toggle_preview",
+  --         ["o"] = "open",
+  --         ["h"] = "close_node",
+  --       },
+  --       fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+  --         ["<C-j>"] = "move_cursor_down",
+  --         ["<C-k>"] = "move_cursor_up",
+  --       },
+  --     },
+  --   },
+  -- },
 }
