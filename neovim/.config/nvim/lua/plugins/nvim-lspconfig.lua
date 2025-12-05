2d668ce44c140ecda87492246a06cf7d97a5dc58
Juan Ramirez <jugaramirez@utp.edu.co>
2025-12-05 15:13:43 -0500

    fix: sonar lint init

--- neovim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,5 +2,18 @@ return {
   "nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      sonarlint_language_server = {
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+        settings = {},
+      },
+    },
   },
 }
